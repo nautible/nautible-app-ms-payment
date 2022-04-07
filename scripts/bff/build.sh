9d27988125369924865c1902160762ab6b63b398
Katsuhiro Yamanaka <Yamanaka_Katsuhiro@ogis-ri.co.jp>
2022-04-07 16:33:32 +0900

    skaffold実行方法変更

--- scripts/bff/build.sh
+++ scripts/bff/build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-cd ../../
 docker build --build-arg HTTP_PROXY=$http_proxy --build-arg HTTPS_PROXY=$http_proxy -t $IMAGE -f ./package/bff/Dockerfile .
---
+++ scripts/bff/skaffold.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd ../../
+skaffold dev --filename=./scripts/bff/skaffold.yaml
